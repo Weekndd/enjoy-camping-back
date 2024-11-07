41e16a847afc732a8fa7913c5e27ad1d90724e7b
Weekndd <hsch19@naver.com>
2024-11-07 16:01:04 +0900

    Refactor: 에러 메세지 수정

--- src/main/java/com/ssafy/enjoycamping/common/response/BaseResponseStatus.java
+++ src/main/java/com/ssafy/enjoycamping/common/response/BaseResponseStatus.java
@@ -17,11 +17,11 @@ public enum BaseResponseStatus {
     ALREADY_EXIST_EMAIL(false, HttpStatus.BAD_REQUEST.value(), "이미 존재하는 이메일입니다."),
     NOT_EXIST_EMAIL(false, HttpStatus.BAD_REQUEST.value(), "존재하지 않는 이메일입니다."),
     NOT_CORRECT_PASSWORD(false, HttpStatus.BAD_REQUEST.value(), "일치하지 않는 비밀번호입니다."),
-    NOT_EXIST_USER(false, HttpStatus.BAD_REQUEST.value(), "존재하지 않는 유저입니다."),
+    NOT_EXIST_USER(false, HttpStatus.NOT_FOUND.value(), "존재하지 않는 유저입니다."),
     WITHDRAW_USER(false, HttpStatus.BAD_REQUEST.value(), "탈퇴한 유저입니다."),
-    NOT_EXIST_CONTENTTYPE(false, HttpStatus.BAD_REQUEST.value(), "존재하지 않는 컨텐츠 타입입니다."),
-    NOT_EXIST_ATTRACTION(false, HttpStatus.BAD_REQUEST.value(), "존재하지 않는 관광지입니다."),
-    NOT_EXIST_CAMPING(false, HttpStatus.BAD_REQUEST.value(), "존재하지 않는 캠핑장입니다."),
+    NOT_EXIST_CONTENTTYPE(false, HttpStatus.NOT_FOUND.value(), "존재하지 않는 컨텐츠 타입입니다."),
+    NOT_EXIST_ATTRACTION(false, HttpStatus.NOT_FOUND.value(), "존재하지 않는 관광지입니다."),
+    NOT_EXIST_CAMPING(false, HttpStatus.NOT_FOUND.value(), "존재하지 않는 캠핑장입니다."),
     
     /**
      * Review
