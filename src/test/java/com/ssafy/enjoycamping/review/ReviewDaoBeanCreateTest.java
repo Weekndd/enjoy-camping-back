c1e21b6ca673e7067b0981ede2383dd48ee28ef6
Weekndd <hsch19@naver.com>
2024-11-07 14:35:11 +0900

    Test: 테스트 책임 분할

--- src/test/java/com/ssafy/enjoycamping/review/ReviewDaoBeanCreateTest.java
+++ src/test/java/com/ssafy/enjoycamping/review/ReviewDaoBeanCreateTest.java
@@ -1,4 +1,4 @@
-package com.ssafy.enjoycamping;
+package com.ssafy.enjoycamping.review;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/com/ssafy/enjoycamping/review/ReviewServiceBeanCreateTest.java
+++ src/test/java/com/ssafy/enjoycamping/review/ReviewServiceBeanCreateTest.java
@@ -1,4 +1,4 @@
-package com.ssafy.enjoycamping;
+package com.ssafy.enjoycamping.review;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,37 +20,9 @@ import com.ssafy.enjoycamping.review.service.ReviewService;
 @SpringBootTest
 class ReviewServiceBeanCreateTest {
 	@Autowired
-//	@Qualifier("reviewServiceImpl")
 	private ReviewService reviewService;
-	@Autowired
-	private ReviewDao reviewDao;
-	
 	@Test
 	void ReviewServiceBeanCreateTest() {
 		assertThat(reviewService).isNotNull();
-	}
-	
-	@Test
-	@Transactional
-	void ReviewCreateTest() {
-		CreateReviewDto.RequestCreateReviewDto request = RequestCreateReviewDto.builder()
-				.campingId(38)
-				.writerId(1)
-				.sidoCode(32)
-				.gugunCode(13)
-				.title("TestTitle3333")
-				.content("TestContent3333")
-				.build();
-		Review review = request.toEntity();
-		assertTrue(review.getTitle()== "TestTitle3333", "successfully inserted");
-		
-		CreateReviewDto.ResponseCreateReviewDto response = reviewService.createReview(request);
-		assertTrue(response.getId() > 0, "successfully inserted");
-	}
-	
-	@Test
-	void selectByIdTest() {
-		ReviewDto reviewDto = reviewService.getReview(11);
-		assertTrue(reviewDto.getId()==11,"성공적으로 조회를 완료했습니다.");
 	}
 }
