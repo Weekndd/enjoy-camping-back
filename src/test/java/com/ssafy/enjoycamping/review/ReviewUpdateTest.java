30bd97fc366e1749b6000443a0371f168052490f
Weekndd <hsch19@naver.com>
2024-11-07 14:32:09 +0900

    Test: 리뷰 수정 테스트 작성

---
+++ src/test/java/com/ssafy/enjoycamping/review/ReviewUpdateTest.java
@@ -0,0 +1,53 @@
+package com.ssafy.enjoycamping.review;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.enjoycamping.common.exception.NotFoundException;
+import com.ssafy.enjoycamping.common.response.BaseResponseStatus;
+import com.ssafy.enjoycamping.review.dao.ReviewDao;
+import com.ssafy.enjoycamping.review.dto.CreateReviewDto;
+import com.ssafy.enjoycamping.review.dto.CreateReviewDto.RequestCreateReviewDto;
+import com.ssafy.enjoycamping.review.dto.UpdateReviewDto.RequestUpdateReviewDto;
+import com.ssafy.enjoycamping.review.dto.UpdateReviewDto;
+import com.ssafy.enjoycamping.review.entity.Review;
+import com.ssafy.enjoycamping.review.service.ReviewService;
+
+@SpringBootTest
+class ReviewUpdateTest {
+	@Autowired
+	ReviewService reviewService;
+	@Autowired
+	ReviewDao reviewDao;
+	
+	@Test
+	@Transactional
+	void reviewUpdateTest() {
+		//GIVEN 
+		CreateReviewDto.RequestCreateReviewDto createRequest = RequestCreateReviewDto.builder()
+				.campingId(38)
+				.writerId(1)
+				.sidoCode(32)
+				.gugunCode(13)
+				.title("reivew 수정 기능 테스트")
+				.content("reivew 수정 기능 테스트")
+				.build();
+		Review originReview = createRequest.toEntity();
+		reviewDao.insert(originReview);
+		
+		//WHEN
+		UpdateReviewDto.RequestUpdateReviewDto updateRequest = new RequestUpdateReviewDto("new Title", "new Content");
+		reviewService.updateReview(updateRequest, originReview.getId());
+		
+		//THEN
+		Review updatedReview = reviewDao.selectById(originReview.getId())
+				.orElseThrow(() -> new NotFoundException(BaseResponseStatus.REVIEW_NOT_FOUND));
+		
+		assertFalse(updatedReview.getTitle() == originReview.getTitle());
+		
+	}
+}
