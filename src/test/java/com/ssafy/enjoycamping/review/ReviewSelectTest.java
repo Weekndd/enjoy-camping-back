78eeb30618e2b4795c63ae27e6ffd7b671d3bf33
Weekndd <hsch19@naver.com>
2024-11-07 16:29:26 +0900

    Test: 리뷰 전체 조회 테스트 작성

--- src/test/java/com/ssafy/enjoycamping/review/ReviewSelectTest.java
+++ src/test/java/com/ssafy/enjoycamping/review/ReviewSelectTest.java
@@ -2,6 +2,8 @@ package com.ssafy.enjoycamping.review;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,5 +22,12 @@ class ReviewSelectTest {
 		ReviewDto reviewDto = reviewService.getReview(11);
 		assertTrue(reviewDto.getId()==11,"성공적으로 조회를 완료했습니다.");
 	}
+	
+	@Test
+	void selectAllTest() {
+		List<ReviewDto> reviews = reviewService.getReviews();
+		int expectedSize = 8; //리뷰수에 맞게 설정 할 것
+		assertTrue(reviews.size() == expectedSize);
+	}	
 
 }
