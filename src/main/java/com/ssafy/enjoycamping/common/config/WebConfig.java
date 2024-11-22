9bdad1e1e72020f6f7c7e9135eab8ccd4ce4929f
jiholee0 <dlwlgh1254@naver.com>
2024-11-22 12:02:58 +0900

    fix: CORS 에러 해결

--- src/main/java/com/ssafy/enjoycamping/common/config/WebConfig.java
+++ src/main/java/com/ssafy/enjoycamping/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
                 .allowedOrigins("http://localhost:5173") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP 메서드
                 .allowCredentials(true); // 쿠키 전송을 허용할지 여부
     }
 }
