a519f932ea1a56edc7e7f7f57a4a40f9d318e066
jiholee0 <dlwlgh1254@naver.com>
2024-11-22 12:03:19 +0900

    fix: 비동기로 S3 삭제 시 지연되는 문제 해결

--- src/main/java/com/ssafy/enjoycamping/common/config/AsyncConfig.java
+++ src/main/java/com/ssafy/enjoycamping/common/config/AsyncConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.enjoycamping.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -9,13 +10,13 @@ import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
-    @Bean
-    public Executor taskExecutor() {
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5); // 최소 스레드 수
-        executor.setMaxPoolSize(10); // 최대 스레드 수
-        executor.setQueueCapacity(500); // 대기열 크기
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(50);
         executor.setThreadNamePrefix("AsyncExecutor-");
         executor.initialize();
         return executor;
